b4cd77a06b7f79e3fb9c44cb59732d0db2e57a57
Carlos <c4rlo4@gmail.com>
2019-10-20 06:59:57 -0300

    predict

--- services/spaceapps/spaceapps/spaceapps/core/utils_ai_preditc.py
+++ services/spaceapps/spaceapps/spaceapps/core/utils_ai_preditc.py
@@ -70,8 +70,8 @@ def ai_predict(imgPath):
     frame2 = np.array(frame2, dtype="float32") / 255
 #     # generating a prdiction of the frame 
 
-    k.clear_session()
-    y_pred = top_model.predict_classes(frame2[None,:,:,:])
+    
+    y_pred = top_model.predict_classes(frame2)
     
     #y_pred = top_model.predict_classes(model.predict(frame2[None,:,:,:]))
     print("y_pred:", y_pred)
