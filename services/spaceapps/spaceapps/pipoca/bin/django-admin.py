3fedfd778e45a7182c72cca6da08ce11da43eeca
Ubuntu <ubuntu@ip-172-31-46-231.us-west-2.compute.internal>
2019-10-23 21:16:42 +0000

    pimba

--- services/spaceapps/spaceapps/pipoca/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/kaspary/spaceapps/services/spaceapps/spaceapps/pipoca/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
